a4afb0c7c144feebe3e38ad3f72bc9297061c4fb
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2026-01-07 21:55:57 +0700

    update auth controller

--- controllers/auth.controller.go
+++ controllers/auth.controller.go
@@ -1,1 +1,31 @@
 package controllers
+
+import (
+	"ecommerce-api/requests"
+	"ecommerce-api/services"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Register(c *gin.Context) {
+	var req requests.AuthRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, err := services.Register(req.Username, req.Password)
+	if err != nil {
+		if err == services.ErrUsernameExists {
+			c.JSON(http.StatusConflict, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "internal error"})
+		return
+	}
+	c.JSON(http.StatusCreated, gin.H{
+		"userId":   user.UserID,
+		"username": user.Username,
+	})
+}
