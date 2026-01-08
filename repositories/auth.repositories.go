efd251151abe34a62cd2b8c772a2a5b1015171b6
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2026-01-08 13:42:53 +0700

    add func FindByUsername in auth repositories

--- repositories/auth.repositories.go
+++ repositories/auth.repositories.go
@@ -9,6 +9,12 @@ func CreateUser(user *models.Auth) error {
 	return config.DB.Create(user).Error
 }
 
+func FindByUsername(username string) (*models.Auth, error) {
+	var user models.Auth
+	err := config.DB.Model(&models.Auth{}).Where("username = ?", username).First(&user).Error
+	return &user, err
+}
+
 func IsUsernameExist(username string) (bool, error) {
 	var count int64
 	err := config.DB.Model(&models.Auth{}).Where("username = ?", username).Count(&count).Error
