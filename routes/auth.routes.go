b4b5a7196214a47af37e5d1e1faaad511b1fee01
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2026-01-08 14:57:22 +0700

    update auth routes

--- routes/auth.routes.go
+++ routes/auth.routes.go
@@ -2,12 +2,14 @@ package routes
 
 import (
 	"ecommerce-api/controllers"
+	"ecommerce-api/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func AuthRoutes(r *gin.Engine) {
 	auth := r.Group("/auth")
+	auth.Use(middleware.RequireAuth)
 	{
 		auth.POST("/register", controllers.Register)
 		auth.POST("/login", controllers.Login)
