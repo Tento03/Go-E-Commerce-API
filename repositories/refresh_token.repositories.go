3cf11d1e1d8dc97f5e09451299a09cfdce450c2b
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2026-01-08 23:15:10 +0700

    add refresh token repositories

---
+++ repositories/refresh_token.repositories.go
@@ -0,0 +1,30 @@
+package repositories
+
+import (
+	"ecommerce-api/config"
+	"ecommerce-api/models"
+	"time"
+)
+
+func SaveRefreshToken(refresh *models.Refresh) error {
+	return config.DB.Create(refresh).Error
+}
+
+func FindValidRefreshToken(token string) (*models.Refresh, error) {
+	var rt models.Refresh
+	err := config.DB.Model(&models.Refresh{}).Where("token = ? AND revoked_at IS NULL", token).First(&rt).Error
+	return &rt, err
+}
+
+func RevokeAllUser(userId string) error {
+	now := time.Now()
+	err := config.DB.Model(&models.Refresh{}).Where("user_id = ?", userId).
+		UpdateColumn("revoked_at", &now).Error
+	return err
+}
+
+func RevokeToken(refresh *models.Refresh) error {
+	now := time.Now()
+	err := config.DB.Model(refresh).UpdateColumn("revoked_at", &now).Error
+	return err
+}
