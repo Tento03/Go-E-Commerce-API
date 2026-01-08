1978480a1b8e11eaaf83b8bba901c84a5971e5bc
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2026-01-08 13:59:24 +0700

    add login function in auth service

--- services/auth.services.go
+++ services/auth.services.go
@@ -5,11 +5,14 @@ import (
 	"ecommerce-api/repositories"
 	"ecommerce-api/utils"
 	"errors"
+	"time"
 
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 var ErrUsernameExists = errors.New("username already exist")
+var ErrInvalidCredentials = errors.New("invalid credentials")
 
 func Register(username string, password string) (*models.Auth, error) {
 	exist, err := repositories.IsUsernameExist(username)
@@ -36,4 +39,19 @@ func Register(username string, password string) (*models.Auth, error) {
 	}
 
 	return user, nil
+}
+
+func Login(username string, password string) (string, error) {
+	user, err := repositories.FindByUsername(username)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return "", ErrInvalidCredentials
+		}
+	}
+
+	if !utils.ComparePassword(user.Password, password) {
+		return "", ErrInvalidCredentials
+	}
+
+	return utils.GenerateToken(user.UserID, user.Username, 15*time.Minute)
 }
