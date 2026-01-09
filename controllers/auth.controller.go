e82da22ebd80c5c4e9287c53bd05714261d656f9
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2026-01-10 00:44:11 +0700

    fix indentation

--- controllers/auth.controller.go
+++ controllers/auth.controller.go
@@ -5,6 +5,7 @@ import (
 	"ecommerce-api/services"
 	"ecommerce-api/utils"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -30,4 +31,62 @@ func Register(c *gin.Context) {
 		"userId":   user.UserID,
 		"username": user.Username,
 	})
+}
+
+func Login(c *gin.Context) {
+	var req requests.AuthRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": utils.ValidationError(err)})
+		return
+	}
+
+	accessToken, refreshToken, err := services.Login(req.Username, req.Password)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		return
+	}
+
+	secured := os.Getenv("APP_ENV") == "production"
+
+	c.SetCookie("accessToken", accessToken, 15*60, "/", "", secured, true)
+	c.SetCookie("refreshToken", refreshToken, 7*24*60*60, "/", "", secured, true)
+
+	c.JSON(http.StatusOK, gin.H{"message": "login success"})
+}
+
+func Refresh(c *gin.Context) {
+	refreshToken, err := c.Cookie("refreshToken")
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "refresh token not found"})
+		return
+	}
+
+	newAccessToken, newRefreshToken, err := services.Refresh(refreshToken)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		return
+	}
+
+	secured := os.Getenv("APP_ENV") == "production"
+
+	c.SetCookie("accessToken", newAccessToken, 15*60, "/", "", secured, true)
+	c.SetCookie("refreshToken", newRefreshToken, 7*24*60*60, "/", "", secured, true)
+
+	c.JSON(http.StatusOK, gin.H{"message": "token refreshed"})
+}
+
+func Logout(c *gin.Context) {
+	refreshToken, err := c.Cookie("refreshToken")
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "refresh token not found"})
+		return
+	}
+
+	_ = services.Logout(refreshToken)
+
+	secured := os.Getenv("APP_ENV") == "production"
+	c.SetCookie("accessToken", "", -1, "/", "", secured, true)
+	c.SetCookie("refreshToken", "", -1, "/", "", secured, true)
+
+	c.JSON(http.StatusOK, gin.H{"message": "logout sukses"})
 }
