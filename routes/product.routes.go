4ebdaefc2cf7a05872c1e774c288e0aa166153e7
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2026-01-10 22:51:15 +0700

    add product routes

---
+++ routes/product.routes.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	controllers "ecommerce-api/controllers/product"
+	"ecommerce-api/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ProductRoutes(r *gin.Engine) {
+	product := r.Group("/product")
+	product.Use(middleware.RequireAuth)
+	{
+		product.GET("/", controllers.FindAll)
+		product.GET("/:id", controllers.FindByTitle)
+		product.GET("/:id", controllers.FindById)
+		product.POST("/", controllers.CreateProduct)
+		product.PUT("/:id", controllers.UpdateProduct)
+		product.DELETE("/:id", controllers.DeleteProduct)
+	}
+}
