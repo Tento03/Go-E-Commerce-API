447973218810017887c608f83017f8c48e9928f4
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2026-01-18 23:42:06 +0700

    update card model

--- models/cart.model.go
+++ models/cart.model.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Cart struct {
 	gorm.Model
-	UserID string `gorm:"not null;uniqueIndex"`
+	UserID string `gorm:"not null"`
 	Items  []CartItem
 }
 
@@ -12,6 +12,6 @@ type CartItem struct {
 	gorm.Model
 	CartID    string
 	ProductID string
-	Qty       string `gorm:"not null"`
+	Qty       int `gorm:"not null"`
 	Product   Product
 }
