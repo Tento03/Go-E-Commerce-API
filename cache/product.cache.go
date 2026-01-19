dabf4e0d0e859e786c42887dec422d6d55146bab
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2026-01-19 22:12:45 +0700

    update product cache

--- cache/product.cache.go
+++ cache/product.cache.go
@@ -2,11 +2,27 @@ package cache
 
 import (
 	"context"
+	"ecommerce-api/config"
 	"ecommerce-api/models"
+	"encoding/json"
 	"time"
 )
 
-type ProductCache interface {
-	GetList(ctx context.Context, key string) ([]*models.Product, error)
-	SetList(ctx context.Context, key string, product []*models.Product, ttl time.Duration) error
+func GetList(ctx context.Context, key string) ([]*models.Product, error) {
+	val, err := config.Client.Get(ctx, key).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	var products []*models.Product
+	if err := json.Unmarshal([]byte(val), &products); err != nil {
+		return nil, err
+	}
+
+	return products, nil
+}
+
+func SetList(ctx context.Context, key string, products []*models.Product, ttl time.Duration) error {
+	bytes, _ := json.Marshal(products)
+	return config.Client.Set(ctx, key, bytes, ttl).Err()
 }
