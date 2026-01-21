8c3a514f78cbb350c6f7fc1c82d7824effaaa98e
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2026-01-21 19:08:31 +0700

    update cart model & and update cart in cart service

--- services/cart.services.go
+++ services/cart.services.go
@@ -17,13 +17,30 @@ func GetCart() (*models.Cart, error) {
 	return cart, nil
 }
 
-func CreateCart(userId string, cartId string, item *[]models.CartItem) (*models.Cart, error) {
+func CreateCart(userId string, cartId string, item *models.CartItem) (*models.Cart, error) {
 	cart := &models.Cart{
 		UserID: userId,
 		Items:  item,
 	}
 
 	if err := repository.CreateCart(cart); err != nil {
+		return nil, err
+	}
+
+	return cart, nil
+}
+
+func UpdateCart(userId string, item *models.CartItem, cartId string, productId string, qty int, product *models.Product) (*models.Cart, error) {
+	cart, err := repository.GetCartById(userId)
+	if err != nil {
+		return nil, err
+	}
+
+	cart.Items.ProductID = productId
+	cart.Items.Qty = qty
+	cart.Items.Product = *product
+
+	if err := repository.UpdateCart(cart); err != nil {
 		return nil, err
 	}
 
